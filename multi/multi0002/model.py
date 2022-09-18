b2db1359580420a30acd0492165fd83944364b37
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-09-18 22:03:28 +0900

    update

--- multi/multi0002/model.py
+++ multi/multi0002/model.py
@@ -7,9 +7,9 @@ class MsciModel(nn.Module):
     def __init__(self, input_channel, output_channel):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Dropout(p=0.8),
             nn.Linear(input_channel, 512),
             nn.ReLU(),
+            nn.Dropout(p=0.8),
             nn.Linear(512, 256),
             nn.ReLU(),
             nn.Linear(256, 128),
