335dba0be2fd74644ab711c155006a6ba2818e13
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-09-19 05:27:43 +0900

    update

---
+++ multi/multi0006/model.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+
+## Model
+class MsciModel(nn.Module):
+    def __init__(self, input_channel, output_channel):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(input_channel, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+            nn.Linear(128, 128),
+            nn.ReLU(),            
+            nn.Dropout(p=0.6),
+            nn.Linear(128, output_channel),
+            nn.Softplus()
+        )
+    
+    def forward(self, x):
+        return self.mlp(x)
