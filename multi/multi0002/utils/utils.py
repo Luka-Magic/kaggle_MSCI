700871b1605da70b96d1c8b92e2f30b883ac3127
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-09-18 23:11:23 +0900

    update

--- multi/multi0002/utils/utils.py
+++ multi/multi0002/utils/utils.py
@@ -109,7 +109,7 @@ class EarlyStopping:
         if self.best_score is None:  #1Epoch目の処理
             self.best_score = score   #1Epoch目はそのままベストスコアとして記録する
             self.checkpoint(score, model)  #記録後にモデルを保存してスコア表示する
-        elif score > self.best_score:  # ベストスコアを更新できなかった場合
+        elif score < self.best_score:  # ベストスコアを更新できなかった場合
             self.counter += 1   #ストップカウンタを+1
             if self.verbose:  #表示を有効にした場合は経過を表示
                 print(f'EarlyStopping counter: {self.counter} out of {self.patience}')  #現在のカウンタを表示する 
