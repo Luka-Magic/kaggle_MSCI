a86e41d7c62416e374ae7839f0f1e7bb85f22f94
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-09-25 21:12:26 +0900

    update

---
+++ cite/cite0008/model.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+
+## Model
+class MsciModel(nn.Module):
+    def __init__(self, cfg, input_channel, output_channel):
+        super().__init__()
+
+        self.mlp = nn.Sequential(
+            nn.Linear(input_channel, int(2**cfg.hidden1)),
+            nn.ReLU(),
+            nn.Linear(int(2**cfg.hidden1), int(2**cfg.hidden2)),
+            nn.ReLU(),            
+            nn.Linear(int(2**cfg.hidden2), int(2**cfg.hidden3)),
+            nn.ReLU(),
+            nn.Linear(int(2**cfg.hidden3), int(2**cfg.hidden4)),
+            nn.ReLU(),
+            nn.Linear(int(2**cfg.hidden4), output_channel),
+            nn.Softplus()
+        )
+    
+    def forward(self, x):
+        return self.mlp(x)
