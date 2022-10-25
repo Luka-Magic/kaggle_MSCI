9421378460d190fbf0b4e90e5707da2c3e7b59e0
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-10-25 21:38:21 +0900

    update

--- cite/cite0009/main.py
+++ cite/cite0009/main.py
@@ -142,13 +142,13 @@ def train_one_epoch(cfg, epoch, train_loader, model, loss_fn, optimizer, schedul
 
     for step, (batch_dict) in pbar:
         if cfg.pca_input is not None:
-            input = batch_dict['input_compressed'].to(cfg.device)
+            input = batch_dict['input_compressed'].to(cfg.device, dtype=torch.float32)
         else:
-            input = batch_dict['input'].to(cfg.device)
+            input = batch_dict['input'].to(cfg.device, dtype=torch.float32)
         if cfg.pca_target is not None:
-            target = batch_dict['target_compressed'].to(cfg.device)
+            target = batch_dict['target_compressed'].to(cfg.device, dtype=torch.float32)
         else:
-            target = batch_dict['target'].to_dense().to(cfg.device)
+            target = batch_dict['target'].to_dense().to(cfg.device, dtype=torch.float32)
         bs = input.shape[0]
 
         optimizer.zero_grad()
@@ -177,14 +177,14 @@ def valid_one_epoch(cfg, epoch, valid_loader, model, pca_train_target_model=None
 
     for step, (batch_dict) in pbar:
         if cfg.pca_input is not None:
-            input = batch_dict['input_compressed'].to(cfg.device)
+            input = batch_dict['input_compressed'].to(cfg.devicee, dtype=torch.float32)
         else:
-            input = batch_dict['input'].to(cfg.device)
+            input = batch_dict['input'].to(cfg.devicee, dtype=torch.float32)
         if cfg.pca_target is not None:
             # target = batch_dict['target_compressed'].to(cfg.device)
-            target = batch_dict['target'].to_dense().to(cfg.device)
+            target = batch_dict['target'].to_dense().to(cfg.devicee, dtype=torch.float32)
         else:
-            target = batch_dict['target'].to_dense().to(cfg.device)
+            target = batch_dict['target'].to_dense().to(cfg.devicee, dtype=torch.float32)
         bs = input.shape[0]
 
         with torch.no_grad():
